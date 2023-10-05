942705e03effef51d8775da229740b96f75020fa
dreammelter <dreammelter@users.noreply.github.com>
2023-10-05 03:50:10 -0700

    #1 - setup visual components for block breaking bits

---
+++ components/block_breaker/cannon_point.gd
@@ -0,0 +1,12 @@
+class_name CannonPoint
+extends Marker2D
+## Marquee for spawning cannon balls; manages its own ammo.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
