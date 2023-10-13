b94cbce5bc36ecf516b719f997e887d22aa95940
dreammelter <dreammelter@users.noreply.github.com>
2023-10-13 11:48:39 -0700

    key items for #7 that i apparently missed

---
+++ components/stats_tools/hit_box.gd
@@ -0,0 +1,10 @@
+class_name HitBox
+extends Area2D
+## Dedicated Area for easy detection of collision w/ various game objects
+## 
+## Bring Your Own Collision Shape
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
